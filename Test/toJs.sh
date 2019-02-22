caf2889cd5cdc13a29ab8f8ce9003003b2b2181d
skatheiya <sachincse2@gmail.com>
2019-02-22 11:33:49 +0530

    Friday 22/02/2019

--- Test/toJs.sh
+++
@@ -1,7 +0,0 @@
-
-
-for file in *.html
-do
-thouch "$file.js"
-sed 's/<[^>]\+>//g' "$file" > "$file.js"
-done
---
+++ demo.js
@@ -0,0 +1,1 @@
+https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=75k3gdbsqbrx1i&scope=r_basicprofile&state=dvchdv&redirect_uri=https://com.mentorz.linkedin.oauth/oauth
